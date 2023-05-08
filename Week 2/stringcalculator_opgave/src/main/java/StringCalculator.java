cfe5717886ba819d62f018bc1fe71e8fd201a0d6
jensbaetens <jens.baetens@ikdoeinformatica.be>
2023-05-08 13:03:22 +0200

    Voorbereiding les 2

---
+++ Week 2/stringcalculator_opgave/src/main/java/StringCalculator.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class StringCalculator {
+    /**
+     * Useful code snippets to use in the implementation
+     */
+    public void usefulCode() {
+        String s="a:b=c";
+        s.split("[:=]");  //returns array {a,b,c}
+        String n="5";
+        Integer.parseInt(n); // returns integer 5
+    }
+
+}
